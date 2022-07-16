0e3647560ed66600aa1a35cc4d4358c4de7d52bd
nickjayesh <80024255+nickjayesh@users.noreply.github.com>
2022-07-16 13:06:41 +0530

    Add hashing method

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import bcrypt
 
+password = 'MyPassWord'
+
+bytePwd = password.encode('utf-8')
+# Generate salt
+# Generate a separate salt for each for better security
+mySalt = bcrypt.gensalt()
+
+# Hash password
+hashedPassword = bcrypt.hashpw(bytePwd, mySalt)
+print(hashedPassword)
